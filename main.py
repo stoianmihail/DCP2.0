introduced cov formula and fixed translation; also avoid the loss on translation

main.py
@@ -82,7 +82,7 @@ def test_one_epoch(args, net, test_loader):
 
         batch_size = src.size(0)
         num_examples += batch_size
-        rotation_ab_pred, translation_ab_pred, rotation_ba_pred, translation_ba_pred, q_z = net(src, target)
+        rotation_ab_pred, translation_ab_pred, rotation_ba_pred, translation_ba_pred, kl_loss = net(src, target)
 
         ## save rotation and translation
         rotations_ab.append(rotation_ab.detach().cpu().numpy())
@@ -104,7 +104,7 @@ def test_one_epoch(args, net, test_loader):
         ###########################
         identity = torch.eye(3).cuda().unsqueeze(0).repeat(batch_size, 1, 1)
         
-        mu, log_var = q_z
+        # mu, log_var = q_z
         # print(f'mu.shape={mu.shape}, log_var.shape={log_var.shape}')
         # print(f'dim=1: {torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1).shape}')
 
@@ -112,13 +112,21 @@ def test_one_epoch(args, net, test_loader):
         # TODO: the MLGS formula has -!!!!!!!!!!!
         # TODO: here it's a - before.
         # TODO: and then applied `+ kl` in `loss`.
-        kl = torch.mean(-0.5 * torch.sum(1 + log_var - log_var.exp(), dim = 1), dim = 0)
-
+        loss = F.mse_loss(torch.matmul(rotation_ab_pred.transpose(2, 1), rotation_ab), identity) \
+            - kl_loss
+            #  + F.mse_loss(translation_ab_pred, translation_ab) \
+            #  - kl_loss
+        
         # kls.append(kl.numpy())
 
         # print(f'kl.shape={kl.shape}, kl={kl}')
-        loss = F.mse_loss(torch.matmul(rotation_ab_pred.transpose(2, 1), rotation_ab), identity) + kl
-        kl_loss = kl
+        # if args.formula == 'diag':
+        #     kl = torch.mean(-0.5 * torch.sum(1 + log_var - log_var.exp(), dim = 1), dim = 0)
+        # elif args.formula == 'cov':
+        #     loss = None
+        # else:
+        #     assert 0, f'Formula {args.formula} not supported!'
+        # kl_loss = kl
         
         
         # loss = F.mse_loss(torch.matmul(rotation_ab_pred.transpose(2, 1), rotation_ab), identity) \
@@ -200,7 +208,7 @@ def train_one_epoch(args, net, train_loader, opt):
         batch_size = src.size(0)
         opt.zero_grad()
         num_examples += batch_size
-        rotation_ab_pred, translation_ab_pred, rotation_ba_pred, translation_ba_pred, q_z = net(src, target)
+        rotation_ab_pred, translation_ab_pred, rotation_ba_pred, translation_ba_pred, kl_loss = net(src, target)
 
         ## save rotation and translation
         rotations_ab.append(rotation_ab.detach().cpu().numpy())
@@ -226,7 +234,7 @@ def train_one_epoch(args, net, train_loader, opt):
         #     q_z, 
         #     torch.distributions.Normal(torch.zeros, 1.)
         # )
-        mu, log_var = q_z
+        # mu, log_var = q_z
         # print(f'mu.shape={mu.shape}, log_var.shape={log_var.shape}')
         # print(f'dim=1: {torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1).shape}')
 
@@ -234,13 +242,16 @@ def train_one_epoch(args, net, train_loader, opt):
         # TODO: the MLGS formula has -!!!!!!!!!!!
         # TODO: here it's a - before.
         # TODO: and then applied `+ kl` in `loss`.
-        kl = torch.mean(-0.5 * torch.sum(1 + log_var - log_var.exp(), dim = 1), dim = 0)
+        # kl = torch.mean(-0.5 * torch.sum(1 + log_var - log_var.exp(), dim = 1), dim = 0)
 
         # kls.append(kl.numpy())
 
         # print(f'kl.shape={kl.shape}, kl={kl}')
-        loss = F.mse_loss(torch.matmul(rotation_ab_pred.transpose(2, 1), rotation_ab), identity) + kl
-        kl_loss = kl
+        loss = F.mse_loss(torch.matmul(rotation_ab_pred.transpose(2, 1), rotation_ab), identity) \
+             - kl_loss
+            #  + F.mse_loss(translation_ab_pred, translation_ab) \
+             
+        # kl_loss = kl
         if args.cycle:
             rotation_loss = F.mse_loss(torch.matmul(rotation_ba_pred, rotation_ab_pred), identity.clone())
             translation_loss = torch.mean((torch.matmul(rotation_ba_pred.transpose(2, 1),
@@ -570,6 +581,8 @@ def train(args, net, train_loader, test_loader, boardio, textio):
 
 def main():
     parser = argparse.ArgumentParser(description='Point Cloud Registration')
+    parser.add_argument('--formula', type=str, default='diag', metavar='N',
+                    help = 'Formula to use[\'diag\', \'cov\']')
     parser.add_argument('--data_size', type=int, default=None, metavar='N',
                     help = 'Data size to use in training')
     parser.add_argument('--data_type', type=str, default=None, metavar='N',

model.py
@@ -490,6 +490,8 @@ class Sprinter(nn.Module):
         super(Sprinter, self).__init__()
         self.emb_dims = args.emb_dims
         self.cycle = args.cycle
+        self.formula = args.formula
+        print(f'Formula!!!!!!!!!!!!!! -> {self.formula}')
         if args.emb_nn == 'pointnet':
             self.emb_nn = PointNet(emb_dims=self.emb_dims)
         elif args.emb_nn == 'dgcnn':
@@ -526,7 +528,7 @@ class Sprinter(nn.Module):
             nn.Linear(self.emb_dims // 2, self.emb_dims // 8),
             MyBatchNorm1d(self.emb_dims // 8),
             nn.LeakyReLU(),
-            nn.Linear(self.emb_dims // 8, 3))
+            nn.Linear(self.emb_dims // 8, 3 if self.formula == 'diag' else 6))
 
     def _refine_with_icp(self, src, tgt, rotation, translation, full_icp=False):
         batch_size = src.size()[0]
@@ -558,6 +560,22 @@ class Sprinter(nn.Module):
         tgt = input[1]
         # debug = input[2]
 
+        src_mean = src.mean(dim=2, keepdim=True)
+        tgt_mean = tgt.mean(dim=2, keepdim=True)
+
+        # print(f'src_mean={src_mean}, shape={src_mean.shape}, ttgt_mnea={tgt_mean}, tgt_mean.shape={tgt_mean.shape}')
+
+        # print(f'src.shape={src.shape}, tgt.shape={tgt.shape}')
+
+        src = (src - src_mean)
+        tgt = (tgt - tgt_mean)
+
+        # print(f'src.shape={src.shape}, tgt.shape={tgt.shape}')
+
+        # print(f'src_mean={src.mean(dim=2, keepdim=True)}')
+
+        assert torch.allclose(src.mean(dim=2, keepdim=True), torch.zeros_like(src_mean, device=src_mean.device), atol=1e-6)
+        assert torch.allclose(tgt.mean(dim=2, keepdim=True), torch.zeros_like(tgt_mean, device=tgt_mean.device), atol=1e-6)
 
         src_embedding = self.emb_nn(src)
         tgt_embedding = self.emb_nn(tgt)
@@ -581,32 +599,72 @@ class Sprinter(nn.Module):
 
         # print(f'combined_embedding={combined_embedding}, shape={combined_embedding.shape}')
 
-        log_var = self.nn(combined_embedding)
-        std = torch.exp(0.5 * log_var)
+        # TODO: implement rotation matrix trick, that we transform the delta angles into rotation matrix.
+        # TODO: then we don't need to apply `rotation_matrix_to_euler_angles` anymore in `self.head` <- more precise + more efficient.
+        # TODO: but we lose the gaussian structure!
 
-        # log_var = torch.zeros_like(log_var)
-        # assert not log_var.requires_grad
+        kl = None
+        if self.formula == 'diag':
+            log_var = self.nn(combined_embedding)
+            std = torch.exp(0.5 * log_var)
 
-        # print(f'std={std}')
+            # log_var = torch.zeros_like(log_var)
+            # assert not log_var.requires_grad
 
-        # print(f'sigma.shape={std}')
-        # print(f'diag={torch.diag(sigma)}')
-        
-        # std = torch.exp(0.5 * logvar)
-        # TODO: wait, is this the same for all?
-        eps = torch.randn_like(std)
-        # print(f'eps={eps.shape}')
-        z = eps * std + torch.rad2deg(mu)
+            # print(f'std={std}')
 
-        # Enforce positive angles <-- what if we're at pi / 2 <-- 
-        z = torch.abs(z)
-        # print(f'z={z}')
-        
-        # dist = torch.distributions.MultivariateNormal(loc=mu, covariance_matrix=torch.diag(sigma))
-        # z = dist.rsample()
-        # print(f'z.shape={z.shape}')
-        rotation_ab = euler_angles_to_rotation_matrix(torch.deg2rad(z), "zyx")
-        
+            # print(f'sigma.shape={std}')
+            # print(f'diag={torch.diag(sigma)}')
+            
+            # std = torch.exp(0.5 * logvar)
+            # TODO: wait, is this the same for all?
+            eps = torch.randn_like(std)
+            # print(f'eps={eps.shape}')
+            z = eps * std + torch.rad2deg(mu)
+
+            # Enforce positive angles <-- what if we're at pi / 2 <-- 
+            z = torch.abs(z)
+            # print(f'z={z}')
+            
+            # dist = torch.distributions.MultivariateNormal(loc=mu, covariance_matrix=torch.diag(sigma))
+            # z = dist.rsample()
+            # print(f'z.shape={z.shape}')
+            rotation_ab = euler_angles_to_rotation_matrix(torch.deg2rad(z), "zyx")
+            kl = -torch.mean(0.5 * torch.sum(log_var.exp() - 1 - log_var, dim = 1), dim = 0)
+        else:
+            log_cholesky = self.nn(combined_embedding)
+            idx = torch.tensor([[0, 1, 2, 1, 2, 2],
+                                [0, 1, 2, 0, 0, 1]], dtype=torch.int64)
+                                # dtype=torch.int64)
+            # TODO: add requires grad.
+            A = torch.zeros((src.shape[0], 3, 3), device=log_cholesky.device)
+            # ret = torch.rand((6,))# if False else torch.tensor([0, 0, 0, -torch.inf, -torch.inf, -torch.inf], dtype=torch.float32)
+            # print(ret.dtype)
+
+            # TODO: do the others entries of `A` need by `np.inf`.
+            A[:, idx[0], idx[1]] = (0.5 * log_cholesky).exp()
+            assert A.requires_grad_
+            # S = A @ A.transpose(1, 2).contiguous()
+
+            # print(f'log_cholesky={log_cholesky}')
+
+            eps = torch.randn_like(mu)
+            assert A.shape[0] == eps.shape[0]
+            # print(f'A.shape={A.shape}, eps.shape={eps.shape}, eps.uns={eps.unsqueeze(-1).shape}')
+            z = torch.rad2deg(mu) + torch.bmm(A, eps.unsqueeze(-1)).squeeze(-1)
+            assert z.shape == mu.shape
+            z = torch.abs(z)
+
+            rotation_ab = euler_angles_to_rotation_matrix(torch.deg2rad(z), "zyx")
+
+            kl = -torch.mean(0.5 * (log_cholesky.exp().sum(dim=1) - log_cholesky[:, :3].sum(dim=1) - 3))
+            # test_kl = -torch.mean(0.5 * torch.sum(log_cholesky.exp() - 1 - log_cholesky, dim = 1), dim = 0)
+            # print(f'kl={kl}, test_kl={test_kl}')
+            # assert torch.isclose(kl, test_kl, atol=1e-6)
+            # test_loss = 0.5 * (S.diagonal(offset=0, dim1=-1, dim2=-2).sum(-1) - torch.log(torch.linalg.det(S)) - 3)
+            # assert torch.allclose(loss, test_loss)
+            # return loss
+
         # print(f'pre_sigma={pre_sigma.shape}')
 
         # input = torch.cat([pre_sigma, mu], 1)
@@ -625,10 +683,23 @@ class Sprinter(nn.Module):
         # curr_num_cands = self.num_cands if self.training else 1
         # z, curr_sigmas, ratio = self.reparameterize(batch_size, curr_num_cands, mu, sigma)
 
+        # TODO: compute translation based on rotation.
+        # TODO: subtract means at the beginning.
+
+    
+        # TODO: this could fail.
         assert not translation_ab.requires_grad
         rotation_ab, translation_ab = self._refine_with_icp(
             src, tgt, rotation_ab, translation_ab
         )
+
+        # TODO: should we reocompute translation?
+        # TODO: yes, since the compute translation would be actually zero!!!
+        # assert torch.allclose(translation_ab, torch.zeros)
+        # print(f'translation_ab[]={translation_ab[0]}\nbefore: {translation_ab.shape}')
+        translation_ab = (torch.matmul(-rotation_ab, src_mean) + tgt_mean).squeeze(2)
+
+        # print(f'trasnlation-ab.shape={translation_ab.shape}')
 
         # TODO: really use this during training?
         # if not self.training:
@@ -639,4 +710,4 @@ class Sprinter(nn.Module):
         rotation_ba = rotation_ab.transpose(2, 1).contiguous()
         translation_ba = -torch.matmul(rotation_ba, translation_ab.unsqueeze(2)).squeeze(2)
 
-        return rotation_ab, translation_ab, rotation_ba, translation_ba, (mu, log_var)
+        return rotation_ab, translation_ab, rotation_ba, translation_ba, kl